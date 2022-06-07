b49eeabdd737eb096c5a633884b4af0e90540260
Kenton Kwok <54623370+kwokkenton@users.noreply.github.com>
2022-06-07 19:46:08 +0100

    Next exercise

--- README.md
+++ README.md
@@ -2,10 +2,14 @@
 
 My attempt at learning processing sequential data: recurrent neural networks (RNNs), time series data and natural language processing.
 
-I am following the Dive Into Deep Learning syllabus (Chapters 8-10) as the primary reference.
+I am following the Dive Into Deep Learning syllabus (Chapters 8-10) as the primary reference. However, I have chosen not to import the entire d2l library and copy out individual functions from the source code instead, as I believe that it would help my understanding. These helper functions are in `utils.py`. 
 
 In addition to running the code, my attempts on the exercises are included. 
 
+Additional relevant materials:
+* Stanford [Lecture 8: Recurrent Neural Networks and Language Models](https://www.youtube.com/watch?v=Keqep_PKrY8&list=PL3FW7Lu3i5Jsnh1rnUwq_TcylNr7EkRe6&index=8) on the issue of vanishing gradients and the chain rule 
+* https://stanford.edu/~shervine/teaching/cs-230/cheatsheet-recurrent-neural-networks
+  
 ## Contents
 `sequence_model.ipynb`: investigation of using a Multi-Layer Perceptron model to forecast a time series. Looks at an autoregressive model to extrapolate a sinusoid using various k-step ahead models. Main takeaway is that errors accumulate when this happens. Refers to Chapter 8.1 of d2l.ai. 
 `text_models.ipynb`: Refers to Chapters 8.2-3 of d2l.ai. 
--- utils.py
+++ utils.py
@@ -1,6 +1,12 @@
 import torch
+from torch import nn
+
 import time
 import numpy as np
+import math
+import matplotlib.pyplot as plt
+from IPython import display
+from matplotlib_inline import backend_inline
 
 
 class Timer:
@@ -80,3 +86,223 @@ def sgd(params, lr, batch_size):
         for param in params:
             param -= lr * param.grad / batch_size
             param.grad.zero_()
+
+
+def use_svg_display():
+    """Use the svg format to display a plot in Jupyter.
+    Defined in :numref:`sec_calculus`"""
+    backend_inline.set_matplotlib_formats('svg')
+
+
+def set_figsize(figsize=(3.5, 2.5)):
+    """Set the figure size for matplotlib.
+    Defined in :numref:`sec_calculus`"""
+    use_svg_display()
+    plt.rcParams['figure.figsize'] = figsize
+
+
+def set_axes(axes, xlabel, ylabel, xlim, ylim, xscale, yscale, legend):
+    """Set the axes for matplotlib.
+    Defined in :numref:`sec_calculus`"""
+    axes.set_xlabel(xlabel), axes.set_ylabel(ylabel)
+    axes.set_xscale(xscale), axes.set_yscale(yscale)
+    axes.set_xlim(xlim),     axes.set_ylim(ylim)
+    if legend:
+        axes.legend(legend)
+    axes.grid()
+
+
+def plot(X, Y=None, xlabel=None, ylabel=None, legend=[], xlim=None,
+         ylim=None, xscale='linear', yscale='linear',
+         fmts=('-', 'm--', 'g-.', 'r:'), figsize=(3.5, 2.5), axes=None):
+    """Plot data points.
+    Defined in :numref:`sec_calculus`"""
+
+    def has_one_axis(X):  # True if `X` (tensor or list) has 1 axis
+        return (hasattr(X, "ndim") and X.ndim == 1 or isinstance(X, list)
+                and not hasattr(X[0], "__len__"))
+
+    if has_one_axis(X):
+        X = [X]
+    if Y is None:
+        X, Y = [[]] * len(X), X
+    elif has_one_axis(Y):
+        Y = [Y]
+    if len(X) != len(Y):
+        X = X * len(Y)
+
+    set_figsize(figsize)
+    if axes is None:
+        axes = plt.gca()
+    axes.cla()
+    for x, y, fmt in zip(X, Y, fmts):
+        axes.plot(x, y, fmt) if len(x) else axes.plot(y, fmt)
+    set_axes(axes, xlabel, ylabel, xlim, ylim, xscale, yscale, legend)
+
+
+class Animator:
+    """For plotting data in animation."""
+
+    def __init__(self, xlabel=None, ylabel=None, legend=None, xlim=None,
+                 ylim=None, xscale='linear', yscale='linear',
+                 fmts=('-', 'm--', 'g-.', 'r:'), nrows=1, ncols=1,
+                 figsize=(3.5, 2.5)):
+        """Defined in :numref:`sec_utils`"""
+        # Incrementally plot multiple lines
+        if legend is None:
+            legend = []
+        use_svg_display()
+        self.fig, self.axes = plt.subplots(nrows, ncols, figsize=figsize)
+        if nrows * ncols == 1:
+            self.axes = [self.axes, ]
+        # Use a lambda function to capture arguments
+        self.config_axes = lambda: set_axes(
+            self.axes[0], xlabel, ylabel, xlim, ylim, xscale, yscale, legend)
+        self.X, self.Y, self.fmts = None, None, fmts
+
+    def add(self, x, y):
+        # Add multiple data points into the figure
+        if not hasattr(y, "__len__"):
+            y = [y]
+        n = len(y)
+        if not hasattr(x, "__len__"):
+            x = [x] * n
+        if not self.X:
+            self.X = [[] for _ in range(n)]
+        if not self.Y:
+            self.Y = [[] for _ in range(n)]
+        for i, (a, b) in enumerate(zip(x, y)):
+            if a is not None and b is not None:
+                self.X[i].append(a)
+                self.Y[i].append(b)
+        self.axes[0].cla()
+        for x, y, fmt in zip(self.X, self.Y, self.fmts):
+            self.axes[0].plot(x, y, fmt)
+        self.config_axes()
+        display.display(self.fig)
+        display.clear_output(wait=True)
+
+# Defined in rnn.ipynb
+
+
+def grad_clipping(net, theta):  # @save
+    """Clip the gradient."""
+    if isinstance(net, nn.Module):
+        params = [p for p in net.parameters() if p.requires_grad]
+    else:
+        params = net.params
+    norm = torch.sqrt(sum(torch.sum((p.grad ** 2)) for p in params))
+    if norm > theta:
+        for param in params:
+            param.grad[:] *= theta / norm
+
+
+def predict(prefix, num_preds, net, vocab, device):
+    """Generate new characters following the 'prefix'."""
+    state = net.begin_state(batch_size=1, device=device)
+    outputs = [vocab[prefix[0]]]
+
+    def get_input(): return torch.tensor(
+        [outputs[-1]], device=device).reshape((1, 1))
+
+    for y in prefix[1:]:  # Warm-up period
+        _, state = net(get_input(), state)
+        outputs.append(vocab[y])
+
+    for _ in range(num_preds):  # Predict `num_preds` steps
+        y, state = net(get_input(), state)
+        outputs.append(int(y.argmax(dim=1).reshape(1)))
+    return ''.join([vocab.idx_to_token[i] for i in outputs])
+
+
+def train_epoch(net, train_iter, loss, updater, device, use_random_iter):
+    """Train a net within one epoch (defined in Chapter 8).
+
+    Args:
+        net
+        train_iter
+        loss (function): loss function
+        updater (function):
+        device (torch.device): 'cpu' or 'cuda', where the tensor will be located
+        use_random_iter (bool): whether random sampling is used
+
+    returns:
+    tuple of metrics
+        perplexity (float) 
+        number of tokens per second/ speed (float)
+
+    """
+    state, timer = None, Timer()
+    metric = Accumulator(2)  # Sum of training loss, no. of tokens
+    for X, Y in train_iter:
+        if state is None or use_random_iter:
+            # Initialize `state` when either it is the first iteration or
+            # using random sampling
+            state = net.begin_state(batch_size=X.shape[0], device=device)
+        else:
+            if isinstance(net, nn.Module) and not isinstance(state, tuple):
+                # `state` is a tensor for `nn.GRU`
+                state.detach_()
+            else:
+                # `state` is a tuple of tensors for `nn.LSTM` and
+                # for our custom scratch implementation
+                for s in state:
+                    s.detach_()
+
+        y = Y.T.reshape(-1)
+        X, y = X.to(device), y.to(device)
+        # Forward propagation
+        y_hat, state = net(X, state)
+        l = loss(y_hat, y.long()).mean()
+
+        # Backpropagation
+        if isinstance(updater, torch.optim.Optimizer):
+            updater.zero_grad()
+            l.backward()
+            grad_clipping(net, 1)
+            updater.step()
+        else:
+            l.backward()
+            grad_clipping(net, 1)
+            # Since the `mean` function has been invoked
+            updater(batch_size=1)
+        metric.add(l * y.numel(), y.numel())
+    return math.exp(metric[0] / metric[1]), metric[1] / timer.stop()
+
+
+def train(net, train_iter, vocab, lr, num_epochs, device,
+          use_random_iter=False):
+    """Train a model (defined in Chapter 8). This wraps the train_epoch function.
+
+    Args:
+        net: neural network
+        train_iter:
+        vocab (vocab instance): text vocabulary
+        lr (float): learning rate
+        num_epochs (int): total number of epochs on which the model is trained on
+        device (torch.device): 'cpu' or 'cuda', where the tensor will be located
+
+    """
+    loss = nn.CrossEntropyLoss()
+    # Animates and plots the learning rate at each training epoch
+    animator = Animator(xlabel='epoch', ylabel='perplexity',
+                        legend=['train'], xlim=[10, num_epochs])
+    # Initialize
+    if isinstance(net, nn.Module):
+        updater = torch.optim.SGD(net.parameters(), lr)
+    else:
+        def updater(batch_size): return sgd(net.params, lr, batch_size)
+
+    # Function which handles this specific prediction
+    def predict_new(prefix): return predict(prefix, 50, net, vocab, device)
+    # Train and predict
+    for epoch in range(num_epochs):
+        ppl, speed = train_epoch(
+            net, train_iter, loss, updater, device, use_random_iter)
+        if (epoch + 1) % 10 == 0:
+            print(predict_new('time traveller'))
+            animator.add(epoch + 1, [ppl])
+
+    print(f'perplexity {ppl:.1f}, {speed:.1f} tokens/sec on {str(device)}')
+    print(predict_new('time traveller'))
+    print(predict_new('traveller'))
